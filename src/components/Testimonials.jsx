87eadde5198bcde2f5cc35eb7214e71f35530ea8
mahade00 <mahade.hassan.bd@gmail.com>
2023-08-13 09:15:44 +0600

    navlink correction

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -4,7 +4,7 @@ import styles from '../Style';
 import FeedBackCard from './FeedBackCard';
 
 const Testimonials = () => (
-    <section id='client' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+    <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
         <div className='absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient'/>
         
         <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
